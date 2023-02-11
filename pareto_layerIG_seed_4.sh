c31dabfcd5b70f14b35dc8023ed4a6a68e6d7c88
Juhyeon Park <parkjh9229@naver.com>
2023-02-11 23:22:26 +0900

    modify command

--- pareto_layerIG_seed_4.sh
+++ pareto_layerIG_seed_4.sh
@@ -1,12 +1,12 @@
 n_sample=2000
 
-for seed in 0 1 2 3 4
+for seed in 4
 do
     for split in 'val'
     do
         for sigma in 0.05
         do
-            for expl in "LayerDL"
+            for expl in "LayerIG"
             do
                 python3 main.py --expl_method $expl --alpha 0.05 --n_sample $n_sample --device 3  \
                 --eval_method "orig" --pred_method "orig" --seed $seed --transform "both" --sigma $sigma --date "${split}_seed_${seed}" \
