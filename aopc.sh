de6a3f34a338465d5f420915c63494d93cd8f16f
Juhyeon Park <parkjh9229@naver.com>
2023-02-19 13:37:56 +0900

    aopc dataparallel

--- aopc.sh
+++ aopc.sh
@@ -3,7 +3,7 @@
 # python3 make_results.py --seed 0 --expl_method "LayerDL" --orig_input_method "center_crop_224" --dataset "center_crop_224" --device 1
 # python3 make_results.py --seed 0 --expl_method "LayerIG" --orig_input_method "center_crop_224" --dataset "center_crop_224" --device 1
 
-python3 aopc.py --seed 0 --device 0 --num_data 1000 --expl_method "GradCAM" --mode "insertion" --tester "ConfAOPC" --perturb_num 250 --perturb_iter 4
+python3 aopc.py --seed 0 --device 0 --num_data 1000 --expl_method "GradCAM" --mode "insertion" --tester "ConfAOPC" --perturb_num 200 --perturb_iter 5
 # python3 aopc.py --seed 0 --device 1 --num_data 1000 --expl_method "LayerXAct" --mode "insertion" --tester "ConfAOPC" --perturb_num 125 --perturb_iter 4
 # python3 aopc.py --seed 0 --device 1 --num_data 1000 --expl_method "LayerDL" --mode "insertion" --tester "ConfAOPC" --perturb_num 125 --perturb_iter 4
 # python3 aopc.py --seed 0 --device 1 --num_data 1000 --expl_method "LayerIG" --mode "insertion" --tester "ConfAOPC" --perturb_num 125 --perturb_iter 4
