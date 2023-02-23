cf472cdf8591d0c1fa6203b578a2a904e12c805f
Juhyeon Park <parkjh9229@naver.com>
2023-02-23 14:19:48 +0900

    20230223 bakcup

--- pareto_layerIG_seed_0_1.sh
+++ pareto_layerIG_seed_0_1.sh
@@ -1,6 +1,6 @@
 n_sample=2000
 
-for seed in 0 1
+for seed in 0 1 2 3 4
 do
     for split in 'val'
     do
@@ -8,7 +8,7 @@ do
         do
             for expl in "LayerIG"
             do
-                python3 main.py --expl_method $expl --alpha 0.05 --n_sample $n_sample --device 0  \
+                CUDA_VISIBLE_DEVICES=0,1 python3 main.py --expl_method $expl --alpha 0.05 --n_sample $n_sample --device 0 1  \
                 --eval_method "orig" --pred_method "orig" --seed $seed --transform "both" --sigma $sigma --date "${split}_seed_${seed}" \
                 --reduction 'none' --sign 'all' --run_option 'pred' --reduction 'sum' --split $split --dataset "center_crop_224" --orig_input_method "center_crop_224" \
                 --batch_size 10
